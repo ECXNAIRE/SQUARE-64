fadc7ffa1383a44589ff8b69e25302d54dca3cd1
suryansh <suryanshsinghrajput007@gmail.com>
2026-06-29 08:34:27 +0530

    added a confirmation popup for the guest option

---
+++ static/js/popupHandle.js
@@ -0,0 +1,17 @@
+const popup = document.getElementById("guestPopup");
+
+const guestBtn = document.getElementById("guestLoginBtn");
+const cancelBtn = document.getElementById("cancelBtn");
+const continueBtn = document.getElementById("continueBtn");
+
+guestBtn.addEventListener("click", () => {
+    popup.classList.add("show");
+});
+
+cancelBtn.addEventListener("click", () => {
+    popup.classList.remove("show");
+});
+
+continueBtn.addEventListener("click", () => {
+    window.location.href = "/main";
+});
